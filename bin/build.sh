171595867395b386a8afe36c885f67ccd4121ca9
Peter Lyons <pete@peterlyons.com>
2024-12-30 22:12:19 -0500

    bin/build.sh installs & runs mise for netlify builds

---
+++ bin/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Please Use Google Shell Style: https://google.github.io/styleguide/shell.xml
+
+# ---- Start unofficial bash strict mode boilerplate
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -o errexit # always exit on error
+set -o errtrace # trap errors in functions as well
+set -o pipefail # don't ignore exit codes when piping output
+set -u # error on reference to unknown variable
+# set -x # enable debugging
+
+IFS=$'\n\t'
+# ---- End unofficial bash strict mode boilerplate
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+if  ! command -v mise >/dev/null; then
+  curl https://mise.run | sh
+fi
+mise run build
